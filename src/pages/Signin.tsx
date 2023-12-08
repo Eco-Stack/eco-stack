b163dfa2dd94bfd40cf234f6ee79b5753cdd4259
Henry-Hong <hhr8540@naver.com>
2023-12-08 23:45:25 +0900

    Feat : 로그인 페이지 수정

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,33 +1,47 @@
-import { useState } from 'react';
-import Svg from 'components/Svg';
 import lottieIconEco from 'assets/lottie_icon_eco.json';
 import Lottie from 'lottie-react';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from 'router';
 
 export default function Signin() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // navigate(ROUTES.projectOverview);
+    // navigate(ROUTES.cloudOverview);
+  };
 
   return (
-    <div className="flex h-screen flex-col justify-center items-center overflow-hidden gap-10 text-white">
-      <h1 className="text-center text-green-400 text-[1.5rem] font-bold translate-y-20 ">Eco Stack</h1>
+    <div className="flex h-[90vh] flex-col justify-center items-center overflow-hidden gap-10 text-white">
+      <h1 className="text-center text-green-400 text-[3rem] font-bold translate-y-20 ">Eco Stack</h1>
       <Lottie animationData={lottieIconEco} />
-      <form className="flex flex-col" onSubmit={e => e.preventDefault()}>
+      <form className="flex flex-col" onSubmit={e => handleSubmit(e)}>
         <label htmlFor="email">Email</label>
         <input
           autoComplete="off"
-          className="min-w-[320px] w-[60%] border-white/40 bg-transparent mb-4 text-white"
+          className="min-w-[320px] rounded w-[60%] border-white/40 bg-transparent mt-1 mb-4 text-white focus:ring-green-400 focus:border-green-400"
           id="email"
           name="email"
+          placeholder="Please enter your email"
         />
 
         <label htmlFor="password">Password</label>
         <input
+          type="password"
           autoComplete="off"
-          className="min-w-[320px] w-[60%] border-white/40 bg-transparent text-white mb-4"
+          className="min-w-[320px] rounded w-[60%] border-white/40 bg-transparent text-white mt-1 mb-4 focus:ring-green-400 focus:border-green-400"
           id="password"
           name="password"
+          placeholder="Please enter your password"
         />
 
-        <input className="min-w-[320px] w-[60%] border-white/40 bg-transparent h-8" type="submit" value="Login"></input>
+        <input
+          className="min-w-[320px] w-[60%] rounded bg-green-400 font-bold text-black h-10 cursor-pointer hover:brightness-95"
+          type="submit"
+          value="Login"
+        ></input>
       </form>
     </div>
   );
